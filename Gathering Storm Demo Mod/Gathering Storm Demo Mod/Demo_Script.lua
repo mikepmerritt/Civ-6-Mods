2163aaa0089bf79815ed1e014d6eb623a6491ebb
mikepmerritt <mikepmerritt@gmail.com>
2022-10-03 01:44:46 -0400

    added some tile picking and reading behavior

--- Gathering Storm Demo Mod/Gathering Storm Demo Mod/Demo_Script.lua
+++ Gathering Storm Demo Mod/Gathering Storm Demo Mod/Demo_Script.lua
@@ -1,5 +1,14 @@
 local function PrintEraChange(previousEra, newEra)
 	print("The era has changed from " .. tostring(previousEra) .. " to " .. tostring(newEra) .. "!")
+	if newEra > 0 and newEra % 2 == 0 then
+		print("This is an era where a location should be selected.")
+		print("Lucky tile is at location (10, 11) for fun.")
+		local luckyPlot = Map.GetPlot(10, 11) -- gets plot at (10, 11)
+		print("Owner of lucky tile: " .. luckyPlot:GetOwner())
+		print("Yield of lucky tile: " .. luckyPlot:GetYield())
+		print("Resource type of lucky tile: " .. luckyPlot:GetResourceType())
+		print("Terrain type of lucky tile: " .. luckyPlot:GetTerrainType())
+	end
 end
 
 local function Initialize()
