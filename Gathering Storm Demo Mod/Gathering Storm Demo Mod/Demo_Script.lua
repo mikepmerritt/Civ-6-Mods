525e01afa200e405a3817fc9da379527cbe445f4
mikepmerritt <mikepmerritt@gmail.com>
2022-10-02 23:59:47 -0400

    added event listener parameters to function for later use

--- Gathering Storm Demo Mod/Gathering Storm Demo Mod/Demo_Script.lua
+++ Gathering Storm Demo Mod/Gathering Storm Demo Mod/Demo_Script.lua
@@ -1,10 +1,10 @@
-local function EraChangePrinter()
-	print("The era has changed!")
+local function PrintEraChange(previousEra, newEra)
+	print("The era has changed from " .. tostring(previousEra) .. " to " .. tostring(newEra) .. "!")
 end
 
 local function Initialize()
 	print("GSDM: Initialized LUA script.")
-	Events.GameEraChanged.Add(EraChangePrinter)
+	Events.GameEraChanged.Add(PrintEraChange)
 end
 
 Initialize()
