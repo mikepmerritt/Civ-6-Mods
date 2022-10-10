f9a106d24d170d4a168b64fcbe77d9858cf44e9e
mikepmerritt <mikepmerritt@gmail.com>
2022-10-10 02:29:02 -0400

    added marked tile property

--- CaptureZones/CaptureZones/CaptureZoneMarking.lua
+++ CaptureZones/CaptureZones/CaptureZoneMarking.lua
@@ -5,8 +5,20 @@ local function GetMapSize()
 	print("Height: " .. mapHeight)
 end
 
+local function MarkPlot(previousEra, newEra)
+	local unmarkedTile = Map.GetPlot(11, 11)
+	local markedTile = Map.GetPlot(10, 11)
+	markedTile:SetProperty("MARKED_SAM", 1)
+	print("Marked tile: " .. (markedTile:GetProperty("MARKED_SAM") or "0"))
+	print("Unmarked tile: " .. (unmarkedTile:GetProperty("MARKED_SAM") or "0"))
+end
+
 local function Initialize()
+	-- map size
 	Events.TurnBegin.Add(GetMapSize)
+
+	-- plot marking
+	Events.GameEraChanged.Add(MarkPlot)
 end
 
 Initialize()
