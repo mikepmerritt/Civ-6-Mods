05779240c011fdabcc1203d8585d64fc3aa552cf
mikepmerritt <mikepmerritt@gmail.com>
2022-10-10 02:48:24 -0400

    restructured a bit for placing marked tiles

--- CaptureZones/CaptureZones/CaptureZoneMarking.lua
+++ CaptureZones/CaptureZones/CaptureZoneMarking.lua
@@ -8,9 +8,21 @@ end
 local function MarkPlot(previousEra, newEra)
 	local unmarkedTile = Map.GetPlot(11, 11)
 	local markedTile = Map.GetPlot(10, 11)
-	markedTile:SetProperty("MARKED_SAM", 1)
-	print("Marked tile: " .. (markedTile:GetProperty("MARKED_SAM") or "0"))
-	print("Unmarked tile: " .. (unmarkedTile:GetProperty("MARKED_SAM") or "0"))
+
+	if newEra == 0 then
+		-- game start
+		markedTile:SetProperty("MARKED_SAM", 1)
+	elseif newEra == 2 then
+		-- medieval
+	elseif newEra == 4 then
+		-- industrial
+	elseif newEra == 6 then
+		-- atomic
+	end
+	
+	-- need or "0" because nil can't be printed or worked with
+	print("Marked tile flag: " .. (markedTile:GetProperty("MARKED_SAM") or 0))
+	print("Unmarked tile flag: " .. (unmarkedTile:GetProperty("MARKED_SAM") or 0))
 end
 
 local function Initialize()
