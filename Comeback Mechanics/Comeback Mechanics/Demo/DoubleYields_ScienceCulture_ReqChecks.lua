304dcee58c4be6e3a554e5317d8ac868c6814d11
mattcmerritt <mattcmerritt@gmail.com>
2022-10-17 03:23:35 -0400

    requirements will activate a check to see how much culture the player has

--- Comeback Mechanics/Comeback Mechanics/Demo/DoubleYields_ScienceCulture_ReqChecks.lua
+++ Comeback Mechanics/Comeback Mechanics/Demo/DoubleYields_ScienceCulture_ReqChecks.lua
@@ -5,61 +5,63 @@ local function GetAllRequirementSets()
         local requirement_sets = GameEffects.GetRequirementSets() or { };
         for i, v in ipairs(requirement_sets) do
             local req_set_def = GameEffects.GetRequirementSetDefinition(v);
-            local subject = GameEffects.GetRequirementSetSubject(v);
-            local subject_name = Locale.Lookup(GameEffects.GetObjectName(subject) or "");
-            local context = GameEffects.GetRequirementSetContext(v);
-            local context_name = Locale.Lookup(GameEffects.GetObjectName(context) or "");
+            local subject_id = GameEffects.GetRequirementSetSubject(v);
+			local subject_name = GameEffects.GetObjectName(subject_id) or "";
+            local context_id = GameEffects.GetRequirementSetContext(v);
+			local context_name = GameEffects.GetObjectName(context_id) or "";
             local state = GameEffects.GetRequirementSetState(v);
-            local inner_count = #(GameEffects.GetRequirementSetInnerRequirements(v) or {});
-            local ref_count = GameEffects.GetRequirementSetReferenceCount(v);
 
-			-- only checking for requirement sets with name PLAYER_FALLS_BEHIND_CULTURE_SAM
-			if (req_set_def.Id == "PLAYER_FALLS_BEHIND_CULTURE_SAM") then
+			-- only checking for requirement sets with name PLAYER_FALLS_BEHIND_CULTURE_SAM and PLAYER_FALLS_BEHIND_SCIENCE_SAM
+			if (req_set_def.Id == "PLAYER_FALLS_BEHIND_CULTURE_SAM" or req_set_def.Id == "PLAYER_FALLS_BEHIND_SCIENCE_SAM") then
 				print("i: " .. i .. " v: " .. v);
 				print("defId: " .. req_set_def.Id);
-				print("subject: " .. subject);
+				print("subject_id: " .. subject_id);
 				print("subject_name: " .. subject_name);
-				print("context: " .. context);
+				print("context_id: " .. context_id);
 				print("context_name: " .. context_name);
 				print("state: " .. state);
-				print("inner_count: " .. inner_count);
-				print("ref_count: " .. ref_count);
+
+				-- check player's culture compared to other players --
+				local living_players = PlayerManager.GetAliveMajors();
+
+				local target_player_index = -1;
+				-- determining which player owns the city --
+				for player_index, player in ipairs(living_players) do
+					local cities = player:GetCities();
+
+					for city_index, city in cities:Members() do
+						local city_name = city:GetName();
+
+						if (city_name == subject_name) then
+							target_player_index = player_index;
+						end
+					end
+				end
+
+                -- initially hoped to see if they were a set amount of civics behind --
+                -- PlayerStats:GetNumCivicsCompleted() was UI only, so an alternative was found --
+				if (target_player_index ~= -1) then
+                    local target_player = living_players[target_player_index];
+                    if (target_player == nil) then
+                        print("Player is null");
+                    elseif (target_player:GetCulture() == nil) then
+                        print("Culture stats are null");
+					
+					elseif (target_player:GetCulture():GetCultureYield() == nil) then
+                        print("Culture yield is null");
+                    else
+                        print("Culture yield: " .. target_player:GetCulture():GetCultureYield());
+                    end
+				else
+					print("Player associated with this city could not be found");
+				end
 			end
         end
     end
 end
 
-local function TestOutput()
-	-- check player's culture compared to other players --
-	local living_players = PlayerManager.GetAliveMajors();
-	print("checking players");
-
-	local target_player_index = -1;
-	-- determining which player owns the city --
-	for player_index, player in ipairs(living_players) do
-		print("player: " .. player_index);
-		local cities = player:GetCities();
-
-		for city_index, city in cities:Members() do
-			-- getting the city name, taken from the live tuner panel City.ltp --
-			local cityName = string.gsub(city:GetName(), "LOC_CITY_NAME_", "");
-			local cityLocName = string.upper(Locale.Lookup(city:GetName()));
-			if ( #cityName > #cityLocName ) then
-				cityName = cityLocName;
-			end
-
-			local cityId = city:GetID();
-            print("\tcity: " .. city_index .. ", city name: " .. cityName .. ", city localized name: " .. cityLocName .. ", city ID: " .. cityId);
-		end
-						
-	end
-
-	-- useful: PlayerStats:GetNumCivicsCompleted() --
-end
-
 local function Initialize()
     Events.TurnEnd.Add(GetAllRequirementSets);
-	Events.TurnBegin.Add(TestOutput);
 end
 
 Initialize()
