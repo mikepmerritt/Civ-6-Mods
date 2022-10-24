07389a5e72191d1ddcc37029846aa3004c54cac2
mikepmerritt <mikepmerritt@gmail.com>
2022-10-24 02:08:40 -0400

    fixed icon not appearing

--- CaptureZones/CaptureZones/CaptureZoneResourceIcons.sql
+++ CaptureZones/CaptureZones/CaptureZoneResourceIcons.sql
@@ -1,6 +1,6 @@
 -- Icon for victory point resource
 -- use toys for now since it isnt used elsewhere really
-INSERT INTO IconDefinitions (Name, Atlas, Index) VALUES
+INSERT INTO IconDefinitions (Name, Atlas, 'Index') VALUES
 ('ICON_RESOURCE_VICTORY_POINTS_SAM', 'ICON_ATLAS_RESOURCES', 37),
 ('RESOURCE_VICTORY_POINTS_SAM', 'ICON_ATLAS_RESOURCES', 37),
 ('ICON_RESOURCE_VICTORY_POINTS_SAM', 'ICON_ATLAS_RESOURCES_FOW', 37);
