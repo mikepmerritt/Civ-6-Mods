83205e3af1d7e83020b8f4748a0f3b4b29bd9572
mikepmerritt <mikepmerritt@gmail.com>
2022-11-07 02:09:03 -0500

    made it work when resource improved once

--- CaptureZones/CaptureZones/CaptureZoneVictory.sql
+++ CaptureZones/CaptureZones/CaptureZoneVictory.sql
@@ -3,18 +3,18 @@
 INSERT INTO Types (Type, Kind) VALUES
 ('VICTORY_CAPTURE_SAM', 'KIND_VICTORY');
 
--- INSERT INTO Requirements (RequirementId, RequirementType, Likeliness, Impact, Reverse, Persistent, ProgressWeight, Triggered) VALUES
--- ('REQUIRES_HAS_ENOUGH_CAPTURE_ZONES_SAM', 'REQUIREMENT_PLOT_PROPERTY_MATCHES', 0, 0, 0, 0, 1, 0);
+INSERT INTO Requirements (RequirementId, RequirementType, Likeliness, Impact, Reverse, Persistent, ProgressWeight, Triggered) VALUES
+('REQUIRES_HAS_ENOUGH_CAPTURE_ZONES_SAM', 'REQUIREMENT_PLAYER_HAS_RESOURCE_OWNED', 0, 0, 0, 0, 1, 0);
 
--- INSERT INTO RequirementArguments (RequirementId, Name, Value) VALUES
--- ('REQUIRES_HAS_ENOUGH_CAPTURE_ZONES_SAM', 'PropertyName', 'CAPTURE_VICTORY_SAM'),
--- ('REQUIRES_HAS_ENOUGH_CAPTURE_ZONES_SAM', 'PropertyMinimum', 1);
+INSERT INTO RequirementArguments (RequirementId, Name, Value) VALUES
+('REQUIRES_HAS_ENOUGH_CAPTURE_ZONES_SAM', 'ResourceType', 'RESOURCE_VICTORY_POINTS_SAM'),
+('REQUIRES_HAS_ENOUGH_CAPTURE_ZONES_SAM', 'Amount', 5);
 
 INSERT INTO RequirementSets (RequirementSetId, RequirementSetType) VALUES
 ('REQUIREMENTS_CAPTURE_VICTORY_SAM', 'REQUIREMENTSET_TEST_ALL');
 
 INSERT INTO RequirementSetRequirements (RequirementSetId, RequirementId) VALUES
-('REQUIREMENTS_CAPTURE_VICTORY_SAM', 'REQUIRES_TECHNOLOGY_CASTLES');
+('REQUIREMENTS_CAPTURE_VICTORY_SAM', 'REQUIRES_HAS_ENOUGH_CAPTURE_ZONES_SAM');
 
 INSERT INTO Victories (VictoryType, Name, Blurb, RequirementSetId) VALUES
 ('VICTORY_CAPTURE_SAM', 'LOC_VICTORY_CAPTURE_SAM_NAME', 'LOC_VICTORY_CAPTURE_SAM_TEXT', 'REQUIREMENTS_CAPTURE_VICTORY_SAM');
