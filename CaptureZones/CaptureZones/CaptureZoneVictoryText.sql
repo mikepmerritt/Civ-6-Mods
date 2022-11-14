24035e1e5ae6b8f835f6473738a2b698d458a1fd
mikepmerritt <mikepmerritt@gmail.com>
2022-11-14 03:40:35 -0500

    added more specific text for each step

--- CaptureZones/CaptureZones/CaptureZoneVictoryText.sql
+++ CaptureZones/CaptureZones/CaptureZoneVictoryText.sql
@@ -11,4 +11,7 @@ INSERT INTO LocalizedText (Language, Tag, Text) VALUES
 ('en_US', 'LOC_VICTORY_CAPTURE_SAM_NAME', 'Control Victory'),
 ('en_US', 'LOC_VICTORY_CAPTURE_SAM_TEXT', 'Your civilization has proven itself to be the strongest and most influential!'),
 ('en_US', 'LOC_VICTORY_CAPTURE_SAM_DESCRIPTION', 'Prove that your civilization is the greatest by completing the Show Strength and Exert Influence projects in the Government Plaza using Victory Points! You can gain Victory Points by controlling the key areas they are placed on later in the game.'),
-('en_US', 'LOC_VICTORY_CAPTURE_SAM_REQUIREMENT', 'hi');
+('en_US', 'LOC_VICTORY_CAPTURE_SHOWN_SUPERIORITY_SAM_REQUIREMENT', 'Show Superiority: [ICON_Bolt]'),
+('en_US', 'LOC_VICTORY_CAPTURE_EXERTED_INFLUENCE_SAM_REQUIREMENT', 'Exert Influence: [ICON_Bolt]');
+
+-- you can use [ICON_CheckmarkBlue] for a checkmark, and [ICON_Bolt] for the little bullet point
